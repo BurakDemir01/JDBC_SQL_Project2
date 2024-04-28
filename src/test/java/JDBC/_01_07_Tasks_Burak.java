9f932e70a501785bc2c710defea58eab982b0fae
burak <burak_demi1@web.de>
2024-04-28 16:39:47 +0200

    DB Utilities is added

--- src/test/java/JDBC/_01_07_Tasks_Burak.java
+++ src/test/java/JDBC/_01_07_Tasks_Burak.java
@@ -1,35 +1,63 @@
-package JDBC;
+package _JBDC;
 
+import JDBC.JDBCParent;
 import org.testng.annotations.Test;
 
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class _01_07_Tasks_Burak extends JDBCParent {
-
     @Test
-    public void Test1() throws SQLException {
-        DBConnectionOpen();
+    public void Test1() {
+        String sorgu="SELECT * FROM employees INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no WHERE dept_no = 'D001'";
+        List<List<String>>  donenTablo=getListData(sorgu);
+        for(List<String> satir : donenTablo) {
+            for (String kolon : satir)
+                System.out.print(kolon+"\t");
 
-        ResultSet rs=sorguEkrani.executeQuery("SELECT * FROM employees INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no WHERE dept_no = 'D001'");
-
-        ResultSetMetaData rsmd= rs.getMetaData();
-
-     // System.out.println("rsmd = " + rsmd.getColumnName(1)+" "+rsmd.getColumnCount() );
-     // for (int i = 1; i <= rsmd.getColumnCount(); i++)
-     //     System.out.print(rsmd.getColumnName(i)+"\t");
-
-     // System.out.println();
-       while (rs.next()){
-
-           for (int i = 1; i <= rsmd.getColumnCount() ; i++)
-               System.out.print(rs.getString(i)+"\t");
-           System.out.println();
-       }
-
-        DBConnectionClose();
+            System.out.println();
+        }
     }
 
+    public List<List<String>> getListData(String sorgu) {
+        List<List<String>> tablo=new ArrayList<>();
 
+        try {
+            DBConnectionOpen();
+            ResultSet rs = sorguEkrani.executeQuery(sorgu);
+            ResultSetMetaData rsmd = rs.getMetaData();
+
+            ArrayList<String> kolonSatiri = new ArrayList<>();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                kolonSatiri.add(rsmd.getColumnName(i));
+            tablo.add(kolonSatiri);
+
+            while (rs.next()) {
+
+                ArrayList<String> satir = new ArrayList<>();
+                // bu satırdaki elemanları satır listine ekle
+                // rs.getString(1); // 1
+                // rs.getString(2); // English
+                for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                    satir.add(rs.getString(i));
+
+                tablo.add(satir);
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            DBConnectionClose();
+        }
+        return tablo;
+    }
+
+    @Test
+    public void Test2(){
+
+    }
 }
---
+++ src/test/java/Utulities/DBUtility.java
@@ -0,0 +1,77 @@
+package Utulities;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBUtility {
+    public static Connection baglanti;
+    public static Statement sorguEkrani;
+
+    public static List<List<String>> getListData(String sorgu)
+    {
+        List<List<String>> tablo=new ArrayList<>();
+
+        try {
+            DBConnectionOpen();
+            ResultSet rs = sorguEkrani.executeQuery(sorgu);
+            ResultSetMetaData rsmd = rs.getMetaData(); // kolon sayısı
+
+            // kolon isimleri tablonun ilk satırına eklendi
+            /*
+            ArrayList<String> kolonSatiri = new ArrayList<>();
+            for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                kolonSatiri.add(rsmd.getColumnName(i));
+            tablo.add(kolonSatiri);
+
+             */
+
+            while (rs.next()) {
+
+                ArrayList<String> satir = new ArrayList<>();
+                // bu satırdaki elemanları satır listine ekle
+                // rs.getString(1); // 1
+                // rs.getString(2); // English
+                for (int i = 1; i <= rsmd.getColumnCount(); i++)
+                    satir.add(rs.getString(i));
+
+                // satıra ekleme bitince tablo ya ekle
+                tablo.add(satir);
+            }
+        }
+        catch(Exception ex)
+        {
+            System.out.println(ex.getMessage());
+        }
+        finally {
+            DBConnectionClose();  // hata olsa d aolmasa da çalış
+        }
+
+        return tablo;
+    }
+
+    public static void DBConnectionOpen()
+    {
+        String url = "jdbc:mysql://db-technostudy.ckr1jisflxpv.us-east-1.rds.amazonaws.com/sakila";
+        String username = "root";
+        String password = "'\"-LhCB'.%k[4S]z";
+
+        try {
+            baglanti = DriverManager.getConnection(url, username, password);
+            sorguEkrani = baglanti.createStatement();
+        }
+        catch(Exception ex)
+        {
+            System.out.println("ex.getMessage() = " + ex.getMessage());
+        }
+    }
+
+    public static void DBConnectionClose()
+    {
+        try {
+            baglanti.close();
+        } catch (SQLException e) {
+            System.out.println("ex.getMessage() = " + e.getMessage());
+        }
+    }
+}

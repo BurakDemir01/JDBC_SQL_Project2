91a2ea802f6e4882e3812fc623017ed41bf02be0
gu1mira <sidagulova@gmail.com>
2024-04-29 09:56:09 +0300

    First initiation

--- src/test/java/JDBC/_36_41_Tasks_Gulmira.java
+++ src/test/java/JDBC/_36_41_Tasks_Gulmira.java
@@ -1,4 +1,11 @@
 package JDBC;
 
-public class _36_41_Tasks_Gulmira extends JDBCParent {
+import Utulities.DBUtility;
+import org.testng.annotations.Test;
+
+public class _36_41_Tasks_Gulmira extends DBUtility {
+    @Test
+    public void Task36(){
+
+    }
 }

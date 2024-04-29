131f4b6c6fabd42c87cb0ffcc126e3ae222051a7
burak <burak_demi1@web.de>
2024-04-29 04:19:08 +0200

    Task 1-7 from automated SQL queries with JDBC formatted

--- src/test/java/JDBC/_01_07_Tasks_Burak.java
+++ src/test/java/JDBC/_01_07_Tasks_Burak.java
@@ -1,19 +1,17 @@
-package _JBDC;
+package JDBC;
 
-import JDBC.JDBCParent;
 import Utulities.DBUtility;
 import org.testng.annotations.Test;
 
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 public class _01_07_Tasks_Burak extends DBUtility {
+
     @Test
     public void Task1() {
-        List<List<String>> dbList = getListData("SELECT * FROM employees INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no WHERE dept_no = 'D001';");
+        List<List<String>> dbList = getListData("SELECT * FROM employees INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no WHERE dept_no = 'D001' LIMIT 22536;");
 
         System.out.println("**** List all employees in department D001 ****");
         System.out.println();
@@ -42,16 +40,21 @@ public class _01_07_Tasks_Burak extends DBUtility {
     }
 
     @Test
-    public void Test3() throws SQLException {
+    public void Task3() throws SQLException {
         DBConnectionOpen();
 
-        ResultSet rs=sorguEkrani.executeQuery("SELECT AVG(salary) AS average_salary FROM salaries;");
+        ResultSet rs = sorguEkrani.executeQuery("SELECT AVG(salary) AS average_salary FROM salaries;");
 
         rs.next();
 
-        System.out.println(rs.getString("average_salary"));
+        System.out.println("**** Calculate the average salary of all employees ****");
+        System.out.println();
+
+        System.out.println(rs.getString(1));
+
 
         DBConnectionClose();
+
     }
 
     @Test
@@ -85,7 +88,7 @@ public class _01_07_Tasks_Burak extends DBUtility {
         DBConnectionOpen();
 
         String sorgu="SELECT e.*, s.salary FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no JOIN salaries s ON e.emp_no = s.emp_no WHERE d.dept_name = 'Sales' AND s.salary > 70000";
-       // ResultSet rs= sorguEkrani.executeQuery("SELECT e.*, s.salary FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no JOIN salaries s ON e.emp_no = s.emp_no WHERE d.dept_name = 'Sales' AND s.salary > 70000;");
+        // ResultSet rs= sorguEkrani.executeQuery("SELECT e.*, s.salary FROM employees e JOIN dept_emp de ON e.emp_no = de.emp_no JOIN departments d ON de.dept_no = d.dept_no JOIN salaries s ON e.e@@mp_no = s.emp_no WHERE d.dept_name = 'Sales' AND s.salary > 70000;");
         //rs.next();
         //System.out.println(rs.getString(1));
 
@@ -101,19 +104,16 @@ public class _01_07_Tasks_Burak extends DBUtility {
     }
 
     @Test
-    public  void  Test7(){
-        DBConnectionOpen();
+    public void Task7() {
+        List<List<String>> dbList = getListData("SELECT e.* FROM employees e INNER JOIN salaries s ON e.emp_no = s.emp_no WHERE s.salary BETWEEN 50000 AND 100000 LIMIT 22536;");
 
-        String sorgu="SELECT e.*\n" +
-                "FROM employees e\n" +
-                "INNER JOIN salaries s ON e.emp_no = s.emp_no\n" +
-                "WHERE s.salary BETWEEN 50000 AND 100000";
+        System.out.println("**** This query retrieves employees who have salaries between 50000 and 100000 ****");
+        System.out.println();
 
-        List<List<String>>  donenTablo=getListData(sorgu);
-        for(List<String> satir : donenTablo) {
-            for (String kolon : satir)
-                System.out.print(kolon+"\t");
-
+        for (int i = 0; i < dbList.size(); i++) {
+            for (int j = 0; j < dbList.get(i).size(); j++) {
+                System.out.print(dbList.get(i).get(j) + " ");
+            }
             System.out.println();
         }
     }
